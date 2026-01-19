2cc4dc8303346e0e92b25f9285fa13fa0fd034e6
Eryk Małkiewicz <eryk.malkiewicz@nolimit.pl>
2026-01-19 16:25:49 +0100

    Day4Part2

---
+++ Day4/part2.py
@@ -0,0 +1,39 @@
+day = 4
+filepath = f'Day{day}\\input.txt'
+input = open(filepath).read()
+lines = input.split('\n')
+
+height = len(lines)
+width = len(lines[0]) 
+
+dx = [1,0,-1]
+dy = [1,0,-1]
+
+area = [[]]
+areastart = [[]]
+sum = 0
+
+for i in range (height+2):
+    area.append([])
+    for j in range (width+2):
+        if i == 0 or i == height+1 or j == 0 or j == width+1:
+            area[i].append('*')
+        else:
+            area[i].append(lines[i-1][j-1])
+
+while area != areastart:
+    areastart = [row[:] for row in area]
+    for y in range(len(area)):
+        for x in range(len(area[y])):
+            if area[y][x] == '@':
+                check = 0
+                for dirx in dx:
+                    for diry in dy:
+                        if area[y+diry][x+dirx] == '@':
+                            check += 1
+                if check <5:
+                    sum += 1
+                    area[y][x] = 'x'
+
+print(sum)
+
